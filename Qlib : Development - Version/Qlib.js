45537569a4dcea688b70f81e14dba43cb7fff925
developers <mouradouchane@gmail.com>
2020-05-24 05:54:20 +0100

    Fixing all path's errors in Dev Version

--- Qlib : Development - Version/Qlib.js
+++ Qlib : Development - Version/Qlib.js
@@ -4,48 +4,48 @@
 // sources
 
 const Console_Sources = [
-    "../Qlib/console/Q_ConsoleLog.js",
-    "../Qlib/console/Q_ConsoleWarn.js",
-    "../Qlib/console/Q_ConsoleError.js",
-    "../Qlib/console/Q_ConsoleInfo.js",
-    "../Qlib/console/Q_ConsoleAssert.js",
-    "../Qlib/console/Q_ConsoleLogStyle.js",
-    "../Qlib/console/Q_ConsoleEmoji.js",
-    "../Qlib/console/Q_ConsoleTable.js",
-    "../Qlib/console/Q_CONSOLE.js"
+    "../Qlib : Development - Version/console/Q_ConsoleLog.js",
+    "../Qlib : Development - Version/console/Q_ConsoleWarn.js",
+    "../Qlib : Development - Version/console/Q_ConsoleError.js",
+    "../Qlib : Development - Version/console/Q_ConsoleInfo.js",
+    "../Qlib : Development - Version/console/Q_ConsoleAssert.js",
+    "../Qlib : Development - Version/console/Q_ConsoleLogStyle.js",
+    "../Qlib : Development - Version/console/Q_ConsoleEmoji.js",
+    "../Qlib : Development - Version/console/Q_ConsoleTable.js",
+    "../Qlib : Development - Version/console/Q_CONSOLE.js"
 ];
 
 const Alert_Sources = [
-    "../Qlib/alert/Q_Alert.js",
+    "../Qlib : Development - Version/alert/Q_Alert.js",
 ];
 
 const Confirm_Sources = [
-    "../Qlib/confirm/Q_Confirm.js",
+    "../Qlib : Development - Version/confirm/Q_Confirm.js",
 ];
 
 const Notification_Sources = [
-    "../Qlib/notification/Q_ButtomBanner.js",
+    "../Qlib : Development - Version/notification/Q_ButtomBanner.js",
 ];
 
 const String_Sources =  [
-    "../Qlib/string/Q_String.js",
-    "../Qlib/string/Q_StringFunctions.js",
+    "../Qlib : Development - Version/string/Q_String.js",
+    "../Qlib : Development - Version/string/Q_StringFunctions.js",
 ];
 
 const Compare_Sources = [
-    "../Qlib/compare/Q_Compare.js",
-    "../Qlib/compare/Q_MultiCompare.js"
+    "../Qlib : Development - Version/compare/Q_Compare.js",
+    "../Qlib : Development - Version/compare/Q_MultiCompare.js"
 ];
 
 const Numbers_Sources = [
-    "../Qlib/numbers/Q_Number.js",
-    "../Qlib/numbers/Q_NumbersFunctions.js",
-    "../Qlib/numbers/Q_NumberSystem.js",
-    "../Qlib/numbers/Q_units.js"
+    "../Qlib : Development - Version/numbers/Q_Number.js",
+    "../Qlib : Development - Version/numbers/Q_NumbersFunctions.js",
+    "../Qlib : Development - Version/numbers/Q_NumberSystem.js",
+    "../Qlib : Development - Version/numbers/Q_units.js"
 ];
 
 const Swap_Sources = [
-    "../Qlib/swap/Q_swap.js"
+    "../Qlib : Development - Version/swap/Q_swap.js"
 ]
 
 function call(ArraySources = []){
